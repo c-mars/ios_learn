3be267b6c5e75992667262c57cf1a7a7f4e0082f
Mars <mars@Besss-Mac-mini-239.local>
2014-07-23 16:30:12 +0300

    Application skeleton

---
+++ SocNetworksApiResearch/SocNetworksApiResearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SocNetworksApiResearch
+//
+//  Created by Mars on 7/23/14.
+//  Copyright (c) 2014 C-Mars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
